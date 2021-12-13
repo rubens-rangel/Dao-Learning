0ee73eed6a74a9d324c889bc56bccba36f27fec4
Rubens Rangel <rubens_rangel@hotmail.com>
2021-12-12 22:44:57 -0300

    second commit

--- src/main/java/Aplication/Program.java
+++ src/main/java/Aplication/Program.java
@@ -1,7 +1,9 @@
 package Aplication;
 
-import Entities.Department;
-import Entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 import java.util.Date;
 
@@ -12,6 +14,9 @@ public class Program {
         System.out.println(obj);
 
         Seller seller = new Seller(21, "bob", "boob@email.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
