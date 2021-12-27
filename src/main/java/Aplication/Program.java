4a318fc980d4f929858b3c85a5d53e76223e7174
rubens.rangel <rubens.rangel@b2wdigital.com>
2021-12-27 17:09:33 -0300

    implementando find by Id

--- src/main/java/Aplication/Program.java
+++ src/main/java/Aplication/Program.java
@@ -10,12 +10,9 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "bob", "boob@email.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
