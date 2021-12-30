196fd53da28f7bfaf25af796b0bcd61d74f06f01
rubens.rangel <rubens.rangel@b2wdigital.com>
2021-12-30 16:59:39 -0300

    delete Implementation

--- src/main/java/Aplication/Program.java
+++ src/main/java/Aplication/Program.java
@@ -7,9 +7,11 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -37,6 +39,17 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=== TEST 5: seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update complete");
 
+        System.out.println("\n=== TEST 5: seller delete =====");
+        System.out.println("type a code to delete:");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Id deleted");
+        sc.close();
     }
 }
